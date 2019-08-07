edb2ed13d0cd24ba5d634db42b75864c4e61f2ea
michael <michael@storeking.id>
2019-08-07 10:28:28 +0700

    Updates

---
+++ precache-manifest.dcaf6d0f4b0560417dec5dfaa0b3a311.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc25b1defaca7ba4dd4e5f70149306e4",
+    "url": "/shopee-test/index.html"
+  },
+  {
+    "revision": "b635dc5824d0ba617ec8",
+    "url": "/shopee-test/static/js/2.49e09e2a.chunk.js"
+  },
+  {
+    "revision": "b74fb6e73baa5fcd6c05",
+    "url": "/shopee-test/static/js/main.88e72dcd.chunk.js"
+  },
+  {
+    "revision": "2bda1944205725075f6e",
+    "url": "/shopee-test/static/js/runtime~main.155181ba.js"
+  }
+]);
